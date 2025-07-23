0083704200c5dcf9a8655272f4cb0bd7d0feb386
Pongsapat P. <tadzyz010@gmail.com>
2025-07-23 08:32:23 +0700

    Make our first page

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const { app, BrowserWindow } = require('electron');
 // ฟังก์ชันสำหรับสร้างหน้าต่าง
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 500,
+    height: 500,
     webPreferences: {
       nodeIntegration: true
     }
