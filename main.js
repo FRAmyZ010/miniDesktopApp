95a7b90307c428b54565eb6c8d2cb2521d07edff
Pongsapat P. <tadzyz010@gmail.com>
2025-07-23 08:19:48 +0700

    Add renderer.js and edit main.js

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-function add(first,second){
-    
-    const answer = first + second;
-    return answer;
+const { app, BrowserWindow } = require('electron');
+
+// ฟังก์ชันสำหรับสร้างหน้าต่าง
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile('index.html'); // โหลด HTML เข้าไปในหน้าต่าง
 }
+
+app.whenReady().then(() => {
+  createWindow(); // สร้างหน้าต่างเมื่อพร้อม
+});
