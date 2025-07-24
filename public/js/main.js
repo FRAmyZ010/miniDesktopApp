d1b695004299e094b221af8eab86683ea539527b
Pongsapat P. <tadzyz010@gmail.com>
2025-07-24 08:29:19 +0700

    Managed file location and add font

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron');
+
+// ฟังก์ชันสำหรับสร้างหน้าต่าง
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 500,
+    height: 500,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile('/views/index.html'); // โหลด HTML เข้าไปในหน้าต่าง
+}
+
+app.whenReady().then(() => {
+  createWindow(); // สร้างหน้าต่างเมื่อพร้อม
+});
